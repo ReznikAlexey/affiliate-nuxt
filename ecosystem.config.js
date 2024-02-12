1b6c42bd01273f372504ceb1deb540517724407e
atagay <atagay@beeline.kz>
2024-02-12 16:24:26 +0600

    added dotenv

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
-module.exports = {
-    apps: [
-      {
-        name: 'Affiliate',
+import {config} from 'dotenv'
+
+config()
+
+export const apps = [
+    {
+        name: 'affiliate',
         port: '3000',
         exec_mode: 'cluster',
         instances: 'max',
         script: './.output/server/index.mjs'
-      }
-    ]
-  }
+    }
+]
