a09ba19b3bcd6ac147bdff4323e4978a70d6b68f
kahnennefer30 <kahennefer@gmail.com>
2024-10-20 23:39:27 +0200

    localizebonus att2

--- pages/offer/[id].vue
+++ pages/offer/[id].vue
@@ -5,6 +5,10 @@
       <h2 class="main-title-m" v-if="currentOffers.title">
         {{ $t(currentOffers.title) }}
       </h2>
+       <!-- Display Localized Bonus -->
+      <h3 class="bonus-text" v-if="currentOffers.bonus">
+        {{ currentOffers.bonus[$i18n.locale] }}
+      </h3>
       <p class="af-text-m af-offer-text" v-if="currentOffers.detail_text" v-html="currentOffers.detail_text[$i18n.locale]">
       </p>
       <div class="points-content">
