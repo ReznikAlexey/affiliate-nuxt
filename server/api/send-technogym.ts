78660cbb1b14343790aa8c6b6f852b687a70f3a3
kahnennefer30 <kahennefer@gmail.com>
2025-05-16 22:48:52 +0200

    added technogym08

--- server/api/send-technogym.ts
+++ server/api/send-technogym.ts
@@ -5,15 +5,15 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   const { name, phone, email } = body
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.yourhost.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'your@email.com',
-      pass: 'yourpassword'
-    }
-  })
+const transporter = nodemailer.createTransport({
+  host: process.env.NUXT_SMTP_HOST,
+  port: Number(process.env.NUXT_SMTP_PORT),
+  secure: false,
+  auth: {
+    user: process.env.NUXT_SMTP_USER,
+    pass: process.env.NUXT_SMTP_PASS
+  }
+})
 
   await transporter.sendMail({
     from: `"Technogym Form" <your@email.com>`,
